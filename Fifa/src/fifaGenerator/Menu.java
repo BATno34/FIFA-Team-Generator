ecea06c6d2326482d57bb3195a6c9e6314fbdef9
BATno34 <ardavanalaei@gmail.com>
2022-05-19 18:06:49 -0400

    lets begin

--- Fifa/src/fifaGenerator/Menu.java
+++ Fifa/src/fifaGenerator/Menu.java
@@ -1,11 +1,16 @@
 package fifaGenerator;
 
 import java.awt.EventQueue;
+import java.awt.SystemColor;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class Menu {
 
-	private JFrame frame;
+	public static JFrame frame;
 
 	/**
 	 *  Launch the application.
@@ -35,8 +40,32 @@ public class Menu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.getContentPane().addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				frame.getContentPane().removeAll();
+				frame.getContentPane().repaint();
+				addComponent();
+			}
+		});
+		frame.getContentPane().setBackground(SystemColor.inactiveCaptionBorder);
+		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
 	}
-
+	
+	/**
+	 * adds frame components to the screen, makes them resizable
+	 */
+	public static void addComponent() {
+		
+		ImageIcon ajax = new ImageIcon("ajax.png");
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(107, 89, 500, 400);
+		lblNewLabel.setText("");
+		lblNewLabel.setIcon(ajax);
+		frame.getContentPane().add(lblNewLabel);
+	}
 }
