3037147e607b08197a19673b41ed2c8242dd44c1
BATno34 <ardavanalaei@gmail.com>
2022-05-20 16:30:21 -0400

    to begin with

--- Fifa/src/fifaGenerator/Menu.java
+++ Fifa/src/fifaGenerator/Menu.java
@@ -1,12 +1,17 @@
 package fifaGenerator;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class Menu {
 
@@ -49,7 +54,7 @@ public class Menu {
 			}
 		});
 		frame.getContentPane().setBackground(SystemColor.inactiveCaptionBorder);
-		frame.setBounds(100, 100, 800, 600);
+		frame.setBounds(200, 50, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -60,12 +65,55 @@ public class Menu {
 	 */
 	public static void addComponent() {
 		
-		ImageIcon ajax = new ImageIcon("ajax.png");
+		JLabel teamOne = new JLabel("");
+		JLabel teamTwo = new JLabel("");
+		JLabel teamThree = new JLabel("");
+		JLabel teamFour = new JLabel("");
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(107, 89, 500, 400);
-		lblNewLabel.setText("");
-		lblNewLabel.setIcon(ajax);
-		frame.getContentPane().add(lblNewLabel);
+		JLabel numberOne = new JLabel("1");
+		numberOne.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 50));
+		numberOne.setBounds(80, 50, 40, 100);
+		frame.getContentPane().add(numberOne);
+		
+		JLabel numberTwo = new JLabel("2");
+		numberTwo.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 50));
+		numberTwo.setBounds(280, 50, 40, 100);
+		frame.getContentPane().add(numberTwo);
+		
+		JLabel numberThree = new JLabel("3");
+		numberThree.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 50));
+		numberThree.setBounds(480, 50, 40, 100);
+		frame.getContentPane().add(numberThree);
+		
+		JLabel numberFour = new JLabel("4");
+		numberFour.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 50));
+		numberFour.setBounds(680, 50, 40, 100);
+		frame.getContentPane().add(numberFour);
+		
+		JButton start = new JButton("Start!");
+		start.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.getContentPane().remove(teamOne);
+				frame.getContentPane().remove(teamTwo);
+				frame.getContentPane().remove(teamThree);
+				frame.getContentPane().remove(teamFour);
+				
+				int generationNumber = (int) (Math.random()*(11)) + 5;
+			}
+		});
+		start.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 25));
+		start.setBounds(325, 400, 150, 75);
+		frame.getContentPane().add(start);
+		//JLabel lblNewLabel = new JLabel("New label");
+		//lblNewLabel.setBounds(107, 89, 500, 200);
+		//lblNewLabel.setText("");
+		
+		//ImageIcon ajax = new ImageIcon("ajax.png");
+		//Image img = ajax.getImage();
+		//Image imgScale = img.getScaledInstance(lblNewLabel.getWidth(), lblNewLabel.getHeight(), Image.SCALE_SMOOTH);
+		//ImageIcon scaledIcon = new ImageIcon(imgScale);
+		
+		//lblNewLabel.setIcon(scaledIcon);
+		//frame.getContentPane().add(lblNewLabel);
 	}
 }
